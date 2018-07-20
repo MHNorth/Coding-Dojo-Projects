c90099b79b71fd8a7d10fe436431c96a2a4dcc10
Maika Northrop <maikanorthrop@gmail.com>
2018-07-20 16:24:08 -0400

    updating django project files

--- DjangoProjects/country_dash/dashapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse, redirect
-  # the index function is called when root is visited
-
-def index(request):
-   
-    
-    return render(request, 'dashapp/templates/login.html')
---
+++ DjangoProjects/sports_orm_II_project/apps/sportsII/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SportsiiConfig(AppConfig):
+    name = 'sportsII'
