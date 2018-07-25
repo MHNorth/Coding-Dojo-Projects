e7f3d9fa2c73d122eedfe2f8ea6d7fb419bec8d1
Maika Northrop <maikanorthrop@gmail.com>
2018-07-25 14:06:13 -0400

    remove files

--- DjangoProjects/blog_project/apps/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('', views.index, name='home'),  # name='home' is often used in html files as links
-        path('about', views.about, name='about'),
-]
--- DjangoProjects/blog_project/apps/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-
-def index(request):
-        return render(request, "blog/index.html")
-
-def about(request):
-        return render(request, "blog/about.html")
